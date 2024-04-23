bc980d2f1a641bc22a654a66aadf2be6439cde01
Blaise Mikka de Gier <ST10249838@vcconnect.edu.za>
2024-04-23 14:55:13 +0200

    Create Update.sql

---
+++ Update.sql
@@ -0,0 +1,5 @@
+UPDATE Venues SET Max_Capacity = 300 WHERE Venue_Code = "V0001";
+UPDATE Venues SET Max_Capacity = 120 WHERE Venue_Code = "V0002";
+UPDATE Venues SET Max_Capacity = 100 WHERE Venue_Code = "V0003";
+UPDATE Venues SET Max_Capacity = 150 WHERE Venue_Code = "V0004";
+UPDATE Venues SET Max_Capacity = 100 WHERE Venue_Code = "V0005";
